ddf24d71f5aa13004d5e067e03de5eb01844a369
Varvara <varya.kuch@gmail.com>
2022-04-13 13:53:31 +0500

    добавление интерактивности для списка сотрудников и формы

--- src/components/employee-item/employees-item.component.jsx
+++ src/components/employee-item/employees-item.component.jsx
@@ -1,28 +1,55 @@
 import React from 'react';
 import './employees-item.styles.css';
 
-const EmployeesItem = ({name, salary, increase}) => {
-  let itemClassNames = 'employees-item';
-
-  if (increase) {
-    itemClassNames += ' increase';
+class EmployeesItem extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      increase: false,
+      rise: false
+    }
   }
-  
-  return (
-    <li className={itemClassNames}>
-      <span className='employees-item__name'>{name}</span>
-      <input type='text' className='employees-item__input' defaultValue={salary + '$'}/>
-      <div className='employees-item__icons'>
-        <button type='button' className='employees-item__button'>
-          <i className='fas fa-cookie'></i>
-        </button>
-        <button type='button' className='employees-item__button'>
-            <i className='fas fa-trash'></i>
-        </button>
-          <i className='fas fa-star'></i>
-      </div>
-    </li>
-  )
+
+  onIncrease = () => {
+    this.setState(({increase}) => ({
+      increase: !increase
+    }));
+  }
+
+  onRise = () => {
+    this.setState(({rise}) => ({
+      rise: !rise
+    }));
+  }
+
+  render() {
+    const {name, salary} = this.props;
+    const {increase, rise} = this.state;
+    let itemClassNames = 'employees-item';
+    if (increase) {
+      itemClassNames += ' increase';
+    }
+
+    if (rise) {
+      itemClassNames += ' like';
+    }
+
+    return (
+      <li className={itemClassNames}>
+        <span className='employees-item__name' onClick={this.onRise}>{name}</span>
+        <input type='text' className='employees-item__input' defaultValue={salary + '$'}/>
+        <div className='employees-item__icons'>
+          <button type='button' className='employees-item__button' onClick={this.onIncrease}>
+            <i className='fas fa-cookie'></i>
+          </button>
+          <button type='button' className='employees-item__button'>
+              <i className='fas fa-trash'></i>
+          </button>
+            <i className='fas fa-star'></i>
+        </div>
+      </li>
+    )
+  }
 }
 
 export default EmployeesItem;
--- src/components/employees-add-form/employees-add-form.component.jsx
+++ src/components/employees-add-form/employees-add-form.component.jsx
@@ -1,17 +1,36 @@
 import React from 'react';
 import './employees-add-form.styles.css';
 
-const EmployeesAddForm = () => {
-  return (
-    <div className='form'>
-      <h3 className="form__title">Добавьте нового сотрудника</h3>
-      <form className="add-form">
-        <input className="add-form__input" type="text" placeholder='Как его зовут?'/>
-        <input className="add-form__input" type="text" placeholder='Зарплата в $'/>
-        <button className="add-form__button" type="submit">Добавить</button>
-    </form>
-    </div>
+class EmployeesAddForm extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      name: '',
+      salary: '',
+    }
+  }
+
+  onInputChange = (evt) => {
+    this.setState(() => ({[evt.target.name]: evt.target.value})); 
+  }
+
+  render() {
+    const {name, salary} = this.state;
+
+    return (
+      <div className='form'>
+        <h3 className="form__title">Добавьте нового сотрудника</h3>
+        <form className="add-form">
+          <input className="add-form__input" type="text" placeholder='Как его зовут?' 
+          name='name' value={name} onChange={this.onInputChange}/>
+          <input className="add-form__input" type="number" placeholder='Зарплата в $' 
+          name='salary' value={salary} onChange={this.onInputChange}/>
+          <button className="add-form__button" type="submit">Добавить</button>
+        </form>
+      </div>
   )
+  }
 }
 
 export default EmployeesAddForm;
